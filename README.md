c9599471077742038da08113267b9b97cd088380
naym00 <naymhsain00@gmail.com>
2025-05-20 01:58:17 +0600

    README.md file added

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# nltk_ai_chat_log
+# AI Chat Log Summarizer
+
+An AI-powered tool that analyzes and summarizes chat logs to extract key insights and important information.
+
+## Installation
+
+Follow these steps to set up and run the project:
+
+### 1. Clone the repository
+```bash
+git clone https://github.com/naym00/nltk_ai_chat_log.git
+cd nltk_ai_chat_log
+
+### 2. Create a virtual environment
+```bash
+python -m venv venv
+
+### 3. Activate the environment(Windows & macOS/Linux)
+```bash
+-Windows
+    -venv\Scripts\activate
+-macOS/Linux
+    -source venv/bin/activate
+
+### 4. Install dependencies
+```bash
+pip install -r requirements.txt
+
+### 5. Run the application
+```bash
+python main.py
--- service/generic.py
+++ service/generic.py
@@ -1,26 +1,22 @@
+from nltk.corpus import stopwords
 from collections import Counter
 import string
 import nltk
 import re
 import os
 
-nltk.download('stopwords')
-from nltk.corpus import stopwords
+nltk.download("stopwords")
+STOP_WORDS = set(stopwords.words("english"))
 
 class Generic:
-    
-    @staticmethod
-    def get_stopwords():
-        return set(stopwords.words('english'))
-    
     @staticmethod
     def clean_and_tokenize(messages: dict[str, list[str]]):
         words = []
-        for msg in messages['user'] + messages['ai']:
+        for msg in messages["user"] + messages["ai"]:
             msg = msg.lower()
             msg = re.sub(rf"[{string.punctuation}]", "", msg)
             tokens = msg.split()
-            words.extend([word for word in tokens if word not in Generic.get_stopwords()])
+            words.extend([word for word in tokens if word not in STOP_WORDS])
         return words
     
     @staticmethod
@@ -31,40 +27,40 @@ class Generic:
     
     @staticmethod
     def prepare_summary(messages: dict[str, list[str]]):
-        total_messages = len(messages['user']) + len(messages['ai'])
+        total_messages = len(messages["user"]) + len(messages["ai"])
         keywords = Generic.keyword_analysis(messages)
 
         summary = f"""
-                    Summary:
-                    - The conversation had {total_messages} exchanges.
-                    - The user asked mainly about {' and '.join(keywords[:2])} and related topics.
-                    - Most common keywords: {', '.join(keywords)}
-                    """
+    Summary:
+    - The conversation had {total_messages} exchanges.
+    - The user asked mainly about {' and '.join(keywords[:2])} and related topics.
+    - Most common keywords: {', '.join(keywords)}
+    """
         print(summary.strip())
     
     @staticmethod
     def parse_chat_log(lines: list[str]):
-        messages = {'user': [], 'ai': []}
+        messages = {"user": [], "ai": []}
         for line in lines:
             if line.startswith("User:"):
-                messages['user'].append(line[5:].strip())
+                messages["user"].append(line[5:].strip())
             elif line.startswith("AI:"):
-                messages['ai'].append(line[3:].strip())
+                messages["ai"].append(line[3:].strip())
         return messages
     
     @staticmethod
     def read_file(path: str):
-        response = {'flag': True, 'lines': []}
+        response = {"flag": True, "lines": []}
         if os.path.exists(path):
             with open(path, "r", encoding="utf-8") as file:
-                response['lines'] = file.readlines()
-        else: response['flag'] = False
+                response["lines"] = file.readlines()
+        else: response["flag"] = False
         return response
     
     @staticmethod
-    def generate_summary(path: str = './static/chat.txt'):
+    def generate_summary(path: str = "./static/chat.txt"):
         file_response = Generic.read_file(path)
-        if file_response['flag']:
-            user_ai_messages = Generic.parse_chat_log(file_response['lines'])
+        if file_response["flag"]:
+            user_ai_messages = Generic.parse_chat_log(file_response["lines"])
             Generic.prepare_summary(user_ai_messages)
-        else: pass
+        else: print("File doesn\'t found!")
