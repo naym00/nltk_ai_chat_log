a87bfadf7cc92d47cce54c316000e0c02795bdf1
naym00 <naymhsain00@gmail.com>
2025-05-19 21:20:17 +0600

    created read_file

--- service/generic.py
+++ service/generic.py
@@ -1,5 +1,20 @@
+import os
+
 class Generic:
     
     @staticmethod
-    def generate_summary():
-        pass
+    def read_file(path: str):
+        response = {'flag': True, 'lines': []}
+        if os.path.exists(path):
+            with open(path, "r", encoding="utf-8") as file:
+                response['lines'] = file.readlines()
+        else: response['flag'] = False
+        return response
+    
+    @staticmethod
+    def generate_summary(path: str = './static/chat.txt'):
+        file_response = Generic.read_file(path)
+        if file_response['flag']:
+            pass
+        else: pass
+        print('file_response ', file_response)
